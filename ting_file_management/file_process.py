ef0f1d118e9bbc7659d0490cc9b1650cda36ac58
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2023-05-08 14:47:00 -0300

    Tentando passar requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file, archive):
+    directory = txt_importer(path_file)
+    create_file = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(directory),
+        'linhas_do_arquivo': directory,
+    }
+
+    for content in archive.data:
+        if content['nome_do_arquivo'] == path_file:
+            return False
+
+    archive.enqueue(create_file)
+
+    return sys.stdout.write(str
+                            (create_file)
+                            )
 
 
 def remove(instance):
