236cb61c4ddd3a191721d4f8e461faadcadda324
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2023-05-08 19:37:28 -0300

    Requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    letters = []
+
+    for content in instance.content:
+        for words in range(len(content['linhas_do_arquivo'])):
+            if word.lower() in content['linhas_do_arquivo'][words].lower():
+                letters.append({'linha': words + 1})
+
+        if len(letters) > 0:
+            return [{
+                'palavra': word,
+                'arquivo': content['nome_do_arquivo'],
+                'ocorrencias': letters
+                }]
+
+        return []
 
 
 def search_by_word(word, instance):
