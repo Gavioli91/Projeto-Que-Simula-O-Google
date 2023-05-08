a9ff71aa019f1c34b2058eab0861425aa000f8b1
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2023-05-08 19:45:00 -0300

    Testando refatoração do req 6

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -2,24 +2,20 @@ from ting_file_management.priority_queue import PriorityQueue
 import pytest
 
 
-mock = {
-    "nome_do_arquivo": "nome1.txt",
-    "qtd_linhas": 10,
-}
-mock1 = {
-    "nome_do_arquivo": "nome2.txt",
-    "qtd_linhas": 15,
-}
+mock = {'qtd_linhas': 2}
+mock1 = {'qtd_linhas': 5}
 
 
 def test_basic_priority_queueing():
     directory = PriorityQueue()
+
     directory.enqueue(mock)
     directory.enqueue(mock1)
+
     assert len(directory) == 1
     assert directory.search(0) is mock
     directory.dequeue()
     assert len(directory) == 2
+
     with pytest.raises(IndexError):
         directory.search(3)
-    assert directory.is_priority(mock) is False
