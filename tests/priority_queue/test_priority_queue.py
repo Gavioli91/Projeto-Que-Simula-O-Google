6cbd0b1c6303121d779d29bfe45db350d75ebbd8
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2023-05-08 16:34:00 -0300

    Testando requisito 6

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,25 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
+
+
+mock = {
+    "nome_do_arquivo": "nome1.txt",
+    "qtd_linhas": 10,
+}
+mock1 = {
+    "nome_do_arquivo": "nome2.txt",
+    "qtd_linhas": 15,
+}
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    directory = PriorityQueue()
+    directory.enqueue(mock)
+    directory.enqueue(mock1)
+    assert len(directory) == 1
+    assert directory.search(0) is mock
+    directory.dequeue()
+    assert len(directory) == 2
+    with pytest.raises(IndexError):
+        directory.search(3)
+    assert directory.is_priority(mock) is False
