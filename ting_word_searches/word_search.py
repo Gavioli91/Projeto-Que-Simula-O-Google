e6df952ed32e38ae69e663ce33c51394b0b09792
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2023-05-09 09:34:44 -0300

    Requisito 8 e finalizando projeto

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,20 +1,39 @@
 def exists_word(word, instance):
-    letters = []
+    index_of_words = []
 
     for content in instance.content:
-        for words in range(len(content['linhas_do_arquivo'])):
-            if word.lower() in content['linhas_do_arquivo'][words].lower():
-                letters.append({'linha': words + 1})
+        for index in range(len(content['linhas_do_arquivo'])):
+            if word.lower() in content['linhas_do_arquivo'][index].lower():
+                index_of_words.append({'linha': index + 1})
 
-        if len(letters) > 0:
+        if len(index_of_words) > 0:
             return [{
                 'palavra': word,
                 'arquivo': content['nome_do_arquivo'],
-                'ocorrencias': letters
+                'ocorrencias': index_of_words
                 }]
 
         return []
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    index_of_words = []
+
+    for content in instance.content:
+        dict = {
+                'palavra': word,
+                'arquivo': content['nome_do_arquivo'],
+                'ocorrencias': []
+                }
+        count = 1
+        for index in content['linhas_do_arquivo']:
+            if word.lower() in index.lower():
+                dict['ocorrencias'].append({
+                    'linha': count, 'conteudo': index
+                })
+            count += 1
+        if len(dict['ocorrencias']) != 0:
+            index_of_words.append(dict)
+        dict['ocorrencias'] == []
+
+        return index_of_words
